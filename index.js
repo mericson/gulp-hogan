6f8b3129cf7c034b1df1677f67954593b47e2d71
Matt Ericson <mericson@ericson.net>
2014-01-30 12:40:46 -0500

    html + partials

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ var es = require('event-stream');;
 var gutil = require('gulp-util');
 var Hogan = require('hogan.js');
 
-module.exports = function(data) {
+module.exports = function(data, partials) {
   data = data || {};
   return es.map(function (file, cb) {
-    file.contents = new Buffer( Hogan.compile(file.contents.toString()).render(data) );
-    file.path = gutil.replaceExtension(file.path, '.js');
+    file.contents = new Buffer( Hogan.compile(file.contents.toString()).render(data, partials) );
+    file.path = gutil.replaceExtension(file.path, '.html');
     cb(null,file);
   });
 };
